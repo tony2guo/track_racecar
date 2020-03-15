679bdf20ac1063e010954dbec3b15bc3cd536709
Shih-Wei Guo <tony2guo@gmail.com>
2020-03-15 22:59:30 +0800

    enable obstacle_avoidance

--- scripts/obstacle_avoidance.py
+++ scripts/obstacle_avoidance.py
@@ -3,16 +3,26 @@
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import Twist
 
+
+def nav_vel_cb(msg):
+    if abs(msg.angular.z) > angular_threshold:
+        cmd_vel_pub.publish(msg)
 
 if __name__ == '__main__': 
     rospy.init_node('obstacle_avoidance')
-    
+
+    angular_threshold = rospy.get_param('~angular_threshold', 0.1)
+
+    cmd_vel_pub = rospy.Publisher("~cmd_vel", Twist, queue_size=1)
+    rospy.Subscriber("nav_vel", Twist, nav_vel_cb)
+
     client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
     client.wait_for_server()
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = 'base_link'
-    goal.target_pose.pose.position.x = 3
+    goal.target_pose.pose.position.x = 4
     goal.target_pose.pose.orientation.w = 1
     while not rospy.is_shutdown():
         rospy.loginfo("send move_base goal")
